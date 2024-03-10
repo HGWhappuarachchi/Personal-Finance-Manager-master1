a8cd8f1d851cdb614021d451853abfcb0d6abbf8
Hapuarachchi H G W <109574496+HGWhappuarachchi@users.noreply.github.com>
2024-03-10 09:24:22 +0530

    FireBaseConnected

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cashapp/Screens/addDetails.dart';
+import 'package:cashapp/Screens/creditScore.dart';
+import 'package:cashapp/Screens/debts.dart';
+import 'package:cashapp/Screens/fingerprint.dart';
+import 'package:cashapp/Screens/report.dart';
+import 'package:cashapp/Screens/user.dart';
+import 'package:cashapp/Screens/welcome.dart';
+import 'package:flutter/material.dart';
+import 'package:cashapp/Screens/details.dart';
+import 'package:cashapp/Screens/home.dart';
+import 'package:cashapp/Screens/Pin.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:FlutterSplashScreen(
+        duration:  const Duration(milliseconds: 2000),
+        nextScreen:HomeScreen(),
+        splashScreenBody: Welcome(),
+      ),
+    ),
+  );
+}
